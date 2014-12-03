7b853f2a170e21e7537396df6e3ce116e1abb062
valentinsvt <valentinsvt@hotmail.com>
2014-12-03 12:23:39 -0500

    prueba email

--- grails-app/conf/Config.groovy
+++ grails-app/conf/Config.groovy
@@ -25,7 +25,7 @@ grails.mime.types = [ html: ['text/html','application/xhtml+xml'],
                       json: ['application/json','text/json'],
                       form: 'application/x-www-form-urlencoded',
                       multipartForm: 'multipart/form-data'
-                    ]
+]
 
 // URL Mapping Cache Max Size, defaults to 5000
 //grails.urlmapping.cache.maxsize = 1000
@@ -75,16 +75,16 @@ log4j = {
     //}
 
     error  'org.codehaus.groovy.grails.web.servlet',  //  controllers
-           'org.codehaus.groovy.grails.web.pages', //  GSP
-           'org.codehaus.groovy.grails.web.sitemesh', //  layouts
-           'org.codehaus.groovy.grails.web.mapping.filter', // URL mapping
-           'org.codehaus.groovy.grails.web.mapping', // URL mapping
-           'org.codehaus.groovy.grails.commons', // core / classloading
-           'org.codehaus.groovy.grails.plugins', // plugins
-           'org.codehaus.groovy.grails.orm.hibernate', // hibernate integration
-           'org.springframework',
-           'org.hibernate',
-           'net.sf.ehcache.hibernate'
+            'org.codehaus.groovy.grails.web.pages', //  GSP
+            'org.codehaus.groovy.grails.web.sitemesh', //  layouts
+            'org.codehaus.groovy.grails.web.mapping.filter', // URL mapping
+            'org.codehaus.groovy.grails.web.mapping', // URL mapping
+            'org.codehaus.groovy.grails.commons', // core / classloading
+            'org.codehaus.groovy.grails.plugins', // plugins
+            'org.codehaus.groovy.grails.orm.hibernate', // hibernate integration
+            'org.springframework',
+            'org.hibernate',
+            'net.sf.ehcache.hibernate'
 
     warn   'org.mortbay.log'
 }
@@ -188,6 +188,9 @@ grails {
         password = "Yachay2014"
         props = ["mail.smtp.starttls.enable":"true",
                  "mail.smtp.port":"25",
+                 "mail.smtp.auth":"true",
                  "mail.debug": "true"]
     }
 }
+//<prop key="mail.smtp.auth">true</prop>
+//       	      <prop key="mail.smtp.starttls.enable">true</prop>
