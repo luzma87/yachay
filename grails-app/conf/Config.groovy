a37b454ed4eceef3f8dfcbc0505f92fd4bea06ac
valentinsvt <valentinsvt@hotmail.com>
2014-12-09 12:19:14 -0500

    prueba email

--- grails-app/conf/Config.groovy
+++ grails-app/conf/Config.groovy
@@ -187,7 +187,7 @@ grails {
         username = "planificacion@yachay.gob.ec"
         password = "Yachay2014"
         props = [
-                "mail.smtp.starttls.enable":"true",
+//                "mail.smtp.starttls.enable":"true",
 //                 "mail.smtp.ssl.enable":"true",
                  "mail.smtp.host":"mail.yachay.gob.ec",
 //                 "mail.smtp.ssl.trust": "mail.yachay.gob.ec",
